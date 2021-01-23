23d6abc14411d2fe740fd5ed4773f9659f41e197
GHeeJeon <jhjeon_99@naver.com>
2021-01-23 19:47:09 +0900

    add: tutorial

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,16 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Gugu BbangdaengYEE' });
+router.get('/', function(req, res) {
+  /**/
+  console.log("이야 요청이 왔다~");
+  console.log("응답을 해주자~");
+  res.render('index', {
+    title: 'Gugu BbangdaengYEE',
+    time: new Date().toISOString(),
+    ip: req.ip
+  }); //응답코드
+
 });
 
 module.exports = router;
