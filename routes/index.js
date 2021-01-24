a65e75b38ab17a0c81da0e557fe4611cd87c181d
GHeeJeon <jhjeon_99@naver.com>
2021-01-25 04:41:29 +0900

    .

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,6 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Gugu BbangdaengYEE' });
-
   console.log("이야 요청이 왔다~");
   console.log("응답을 해주자~");
 
