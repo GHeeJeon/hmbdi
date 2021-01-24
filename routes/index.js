f312eb31ce43701ffa3cd6f670b41fc24248c511
GHeeJeon <jhjeon_99@naver.com>
2021-01-25 04:14:28 +0900

    I don't know that to do

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,18 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function(req, res) {
   res.render('index', { title: 'Gugu BbangdaengYEE' });
+
+  console.log("이야 요청이 왔다~");
+  console.log("응답을 해주자~");
+
+  res.render('index', {
+    title: '단어시험',
+    time: new Date().toISOString(),
+    ip: req.ip
+  }); //응답코드
+
 });
 
 module.exports = router;
